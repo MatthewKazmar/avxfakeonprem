35898610cfde68729dbfef124725608f106d9604
Matt Kazmar <mkazmar@aviatrix.com>
2022-08-24 13:43:49 -0500

    template5

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 resource "aws_eip" "vpn_vm_eip" {}
 
 resource "random_string" "psk" {
-  length = 16
+  length  = 16
   special = false
 }
 
@@ -18,7 +18,7 @@ resource "aviatrix_transit_external_device_conn" "fake_onprem" {
   bgp_local_as_num  = var.transit_gw_asn
   bgp_remote_as_num = var.fake_onprem_asn
   remote_gateway_ip = aws_eip.vpn_vm_eip.address
-  pre_shared_key = resource.random_string.result
+  pre_shared_key    = resource.random_string.psk.result
 }
 
 # AWS VPC for StrongSwan/FRR
@@ -111,7 +111,7 @@ module "vpn_vm" {
 echo "s/:gwname:/${var.transit_gw_name}/g"
 echo "s/:gw:/${var.transit_gw_ips[0]}/g"
 echo "s/:hagw:/${var.transit_gw_ips[1]}/g"
-echo "s/:psk:/${resource.random_string.result}/g"
+echo "s/:psk:/${resource.random_string.psk.result}/g"
 echo "s/:remote-as:/${var.fake_onprem_asn}/g"
 echo "s/:myprivateip:/$(ec2metadata --local-ipv4)/g"
 echo "s/:mypublicip:/$(curl ifconfig.me)/g"
