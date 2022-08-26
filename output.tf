008ee91293fd40495293f0f721e74bca39bc0cd2
Matt Kazmar <mkazmar@aviatrix.com>
2022-08-26 10:08:26 -0500

    typo

--- output.tf
+++ output.tf
@@ -3,7 +3,7 @@ output "vpn_vm_ip" {
   value       = module.vpn_vm_eip.public_ip
 }
 
-output "vpn_vm_ip" {
+output "test_vm_ip" {
   description = "Public IP of Test VM"
   value       = module.test_vm.public_ip
 }
